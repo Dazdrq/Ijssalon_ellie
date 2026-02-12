46ee381f1841efea917e4606ebcf01696ed0fb89
Nick Lambrechts <nicklambrechts1984@hotmail.com>
2026-02-12 20:07:09 +0100

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,22 +1,27 @@
-#Opdracht 2
-prijzen={
-    "aarbei":3,
-    "vanille":4,
-    "chocolade":5
-}
-#Opdracht 3
-aanbieding=prijzen["aarbei"]*0.8
-#Opdracht 4
-reclame_tekst= f"vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
-#Opdracht 5
-reclame_tekst2=reclame_tekst[:62]
-#Opdracht 6
-reclame_tekst3=reclame_tekst2.upper()
-#Opdracht 7
-reclame_tekst4=reclame_tekst3.split(" ")
-#Opdracht 8,9,10
-for el in reclame_tekst4:
-    if len(el)>4:
-        print(el.upper())
-    else: 
-        print(el.lower())
+from helper import decoreer
+
+def print_aanbieding():
+    prijzen={
+        "aarbei":3,
+        "vanille":4,
+        "chocolade":5
+    }
+   
+    aanbieding=prijzen["aarbei"]*0.8
+  
+    reclame_tekst= f"vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
+    
+    reclame_tekst2=reclame_tekst[:63]
+   
+    reclame_tekst3=reclame_tekst2.upper()
+
+    reclame_tekst4=reclame_tekst3.split(" ")
+    
+    for el in reclame_tekst4:
+        if len(el)>4:
+            print(el.upper())
+        else: 
+            print(el.lower())
+
+decoreer("Aanbieding")
+print_aanbieding()  
